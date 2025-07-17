7cc679898a8478603b5a3e55ab3de89e0f5937f4
mantis-has <mantisbotmd@gmail.com>
2025-07-17 17:45:36 -0400

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -13,13 +13,13 @@ const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]
 let bot = global.db.data.settings[this.user.jid] || {};
 const isGroupLink = linkRegex1.exec(m.text) || linkRegex2.exec(m.text);
 const grupo = `https://chat.whatsapp.com`;
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(await tr('*El AntiLink Esta activo pero que salvarte eres admin 😎!*'));
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(await tr('*El AntiLink Esta activo, eres admin, perdonado por ahora.*'));
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
 if (m.text.includes(linkThisGroup)) return !0;
 }
-conn.sendMessage(m.chat, { text: `*「 ${await tr("ANTILINK DETECTADO")} 」*\n\n${user} 🤨 ${await tr("Rompiste las reglas del Grupo sera eliminado....")}`, mentions: [m.sender] }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100 });
+conn.sendMessage(m.chat, { text: `*「 ${await tr("ANTILINK DETECTADO")} 」*\n\n${user} ${await tr("Rompiste las reglas del Grupo sera eliminado....")}`, mentions: [m.sender] }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100 });
 if (!isBotAdmin) return conn.sendMessage(m.chat, { text: await tr(`*Te salvarte gil, no soy admin no te puedo eliminar*`), mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
 if (isBotAdmin) {
 conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
